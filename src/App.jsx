a36da8964b5124aebad884ba56a22d705929230e
Nethma Kalpani <Kalpani4lk@gmail.com>
2024-01-13 23:33:58 -0500

    Dynamically change the snow color with thetheme

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,38 @@ import Contact from "./pages/Contact.jsx";
 import Error404 from "./pages/404.jsx";
 import Snowfall from "react-snowfall";
 import About from "./pages/About.jsx";
+import { useTheme } from "./hooks/ThemeProvider.jsx";
 
 function App() {
+    const { currentTheme } = useTheme();
+
     return (
-        <>
-            <Snowfall
-                snowflakeCount={50}
-                radius={[0.5, 0.5]}
-                speed={[1, 2]}
-                wind={[-0.5, 0.5]}
-                color={"#ffffff"}
-                style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}
-            />
-            <div className="bg-white dark:bg-black">
-                <Routes>
-                    <Route path={"/"} element={<Home/>}/>
-                    <Route path={"about"} element={<About/>}/>
-                    <Route path={"projects"} element={<Projects/>}/>
-                    <Route path={"resume"} element={<Resume/>}/>
-                    <Route path={"contact"} element={<Contact/>}/>
-                    <Route path={"*"} element={<Error404/>}/>
-                </Routes>
-            </div>
-        </>
+      <>
+        <Snowfall
+          snowflakeCount={50}
+          radius={[1, 1]}
+          speed={[1, 2]}
+          wind={[-0.5, 0.5]}
+          color={currentTheme === "dark" ? "#FFF" : "#000"}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        />
+        <div className="bg-white dark:bg-black">
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"about"} element={<About />} />
+            <Route path={"projects"} element={<Projects />} />
+            <Route path={"resume"} element={<Resume />} />
+            <Route path={"contact"} element={<Contact />} />
+            <Route path={"*"} element={<Error404 />} />
+          </Routes>
+        </div>
+      </>
     );
 }
 
